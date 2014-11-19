43d256189ddb3a118c02723eb07b36bda9ccebcc
Maxime Bedard <maxim3.bedard@gmail.com>
2014-11-19 15:38:21 -0500

    Added path form, new and edit views

--- app/helpers/panel_helper.rb
+++ app/helpers/panel_helper.rb
@@ -31,5 +31,26 @@ module PanelHelper
     }[type] || type.to_s
   end
 
+  def errors_for(record, options = {})
+
+    return if record.errors.empty?
+
+    "<div class='alert alert-warning alert-dismissible' role='alert'>
+      <button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button>
+      <ul>
+       #{record.errors.full_messages.collect {|msg| "<li>#{msg}</li>"}.join()}
+      </ul>
+    </div>".html_safe
+
+  end
+
+   def back_to(url = nil, options = {})
+    options.reverse_update({
+      class:"btn btn-default"
+    })
+    link_to "Back", url.nil? ? request.referer : url, options
+  end
+
+
 
 end

619d249329bdba1163698cb1947c38180cd104d0
Marc LeBlanc <thewhite147@gmail.com>
2014-11-19 17:49:34 -0500

    Create form done.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,9 @@
 class Client < ActiveRecord::Base
   include Addressable
 
-  attr_accessible :name, :description
+  attr_accessible :name, :description, :estimated_delivery_time, :delivery_item
+
+  validates_numericality_of :estimated_delivery_time
 
   has_and_belongs_to_many :path
 

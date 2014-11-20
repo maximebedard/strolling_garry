b1ca3dd443d7e7a5a0349a14ca6acfa20838b6f3
Maxime Bedard <maxim3.bedard@gmail.com>
2014-11-20 17:21:58 -0500

    Added unobstrusive js for radio button groups

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,17 @@
 //= require jquery_ujs
 //= require bootstrap/dist/js/bootstrap
 
-/// test
+$(document).ready(function(){
+
+  // Toogle radio buttons groups
+  $("[data-toggle='buttons'] .btn").each(function(i, el) {
+    var $button = $(el);
+    var checked = $button.find("input[type='radio']").prop("checked");
+    if (checked) {
+      $button.addClass("active");
+    } else {
+      $button.removeClass("active");
+    }
+  });
+
+});

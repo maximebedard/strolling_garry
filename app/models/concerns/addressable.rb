366d797958b42becdf37cbfc969430b67b1dbaf6
Maxime Bedard <maxim3.bedard@gmail.com>
2014-11-19 22:07:00 -0500

    Updated bundle, added nested attributes to addressable

--- app/models/concerns/addressable.rb
+++ app/models/concerns/addressable.rb
@@ -2,8 +2,8 @@ module Addressable
   extend ActiveSupport::Concern
 
   included do
-    has_one :address, as: :addressable
-    #attr_accessible :address
+    has_one :address, as: :addressable, dependent: :delete
+    accepts_nested_attributes_for :address
   end
 
 end
